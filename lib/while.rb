613446ce83ab8a02e4d9a40fe2ab564d86105211
teresita cisneros estrada <cisneros0831@gmail.com>
2020-06-09 04:08:46 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -3,7 +3,8 @@ def using_while
 	
   #your code here
   while levitation_force != 10
-  puts "Wingardiym Leviosa"
+  puts "Wingardium Leviosa"
+  levitation_force += 1 
 end 
 end
 
@@ -11,8 +12,8 @@ end
 # def using_until
   # levitation_force = 6
   #your code here
-    until levitation_force == 10
-    puts "Wingardium Leviosa"
-    levitation_force += 1 
-  end
-end
+    # until levitation_force == 10
+    # puts "Wingardium Leviosa"
+    # levitation_force += 1 
+  # end
+# end
